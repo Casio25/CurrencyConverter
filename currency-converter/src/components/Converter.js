9b08926aa352fd0e15391dc88f04d7a117fefa0b
casio25 <mishakolomiets355@gmail.com>
2023-04-30 01:40:09 +0300

    bad css

--- currency-converter/src/components/Converter.js
+++ currency-converter/src/components/Converter.js
@@ -176,20 +176,16 @@ export const ConverterBlock = observer(() => {
                 <div>
                     <button className="clear_story_btn" onClick={clearHistory}>Очистити історію</button>
                 </div>
+                {HistoryStore.historyArray.map((item, index) => (
                 <div className="convertor_story_container">
-                    <div className="convertor_story_operetion">
-                        <div className="convertor_story_item">
-                            {HistoryStore.historyArray.map((item, index) => (
-                                <div key={index} className="convertor_story_operetion">
+                                <div key={index} className="convertor_story_elem">
                                     <h5 className="convertor_operetion_date">{item.date}</h5>
                                     <h5 className="convertor_operetion_sum">{item.leftValue} {item.leftCurrency}</h5>
                                     <img className="arrow_img" src={arrow} alt="arrow" />
                                     <h5 className="converted_operation_sum">{item.rightValue} {item.rightCurrency}</h5>
                                 </div>
+                            </div>
                             ))}
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     );
